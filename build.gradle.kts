ef1431ace6a9555917e8f7348e3e5f1642fc0ec1
david steinsland <david@davidsteinsland.net>
2024-11-12 20:58:02 +0100

    bumper versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 val junitJupiterVersion = "5.11.3"
-val rapidsAndRiversVersion = "2024111211071731406062.648687519469"
-val tbdLibsVersion = "2024.11.12-11.09-16cf2599"
+val rapidsAndRiversVersion = "2024111220531731441232.6f0a7a6c643b"
+val tbdLibsVersion = "2024.11.12-20.46-e1fd6a25"
 val mainClass = "no.nav.helse.AppKt"
 
 repositories {
