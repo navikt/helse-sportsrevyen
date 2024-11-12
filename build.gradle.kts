0e93812ae2222799cec85a5ca426cd5be6f23289
david steinsland <david@davidsteinsland.net>
2024-11-12 11:04:28 +0100

    bumper junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-val junitJupiterVersion = "5.10.2"
+val junitJupiterVersion = "5.11.3"
 val rapidsAndRiversVersion = "2024110520091730833744.40f3b6aa7090"
 val tbdLibsVersion = "2024.11.06-12.37-f870e704"
 val mainClass = "no.nav.helse.AppKt"
