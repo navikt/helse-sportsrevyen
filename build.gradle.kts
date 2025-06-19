4f6994909b234fdf0cfbe51905ca8548c25559c9
david steinsland <david@davidsteinsland.net>
2025-06-19 08:36:33 +0200

    bumper r&r og legger til team-logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2025033014191743337188.2f9d6b08d096"
+val rapidsAndRiversVersion = "2025061811051750237542.df739400e55e"
 val tbdLibsVersion = "2025.03.30-14.11-a91ce546"
 val hikariCPVersion = "6.3.0"
 val flywayVersion = "11.5.0"
