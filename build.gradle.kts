cb600c777670dbd0cbd3ba03dab146b473033eea
david steinsland <david@davidsteinsland.net>
2025-08-16 14:03:44 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2025080710011754553680.051be9b54ef9"
+val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
 val hikariCPVersion = "6.3.0"
 val flywayVersion = "11.5.0"
