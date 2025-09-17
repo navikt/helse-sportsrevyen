e4a66f487ea8b31dc291684e63e4b3bc6ca70474
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-17 13:08:54 +0200

    Bytter baseimage og bumper dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
 val hikariCPVersion = "6.3.0"
 val flywayVersion = "11.5.0"
-val postgresqlVersion = "42.7.5"
+val postgresqlVersion = "42.7.7"
 val mainClass = "no.nav.helse.AppKt"
 
 repositories {
