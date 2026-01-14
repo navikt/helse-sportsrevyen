8bc8d2a4d6c27987abc8b996f045a34bd9580b2c
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-01-14 11:28:19 +0100

    Bumper rapids and rivers over hele fjøla

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2025110410191762247980.5e0592e08597"
+val rapidsAndRiversVersion = "2026011411051768385145.e8ebad1177b4"
 val tbdLibsVersion = "2025.11.04-10.12-3a167132"
 val hikariCPVersion = "6.3.0"
 val flywayVersion = "11.5.0"
